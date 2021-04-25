06528efd29ef8cc692e08f6c5e42a6b94f422e05
modione <unconfigured@null.spigotmc.org>
2021-04-25 13:36:04 +0200

    Initial commit

---
+++ src/main/java/me/sm/apvpplugin/ApvpPlugin.java
@@ -0,0 +1,17 @@
+package me.sm.apvpplugin;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class ApvpPlugin extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
