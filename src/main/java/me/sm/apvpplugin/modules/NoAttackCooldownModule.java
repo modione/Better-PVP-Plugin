1584b77e659cb9675e9e0d1c7d9a5e0cd8199c16
someron <devsomeron@gmail.com>
2021-04-25 16:35:39 +0200

    Removed unused imports

--- src/main/java/me/sm/apvpplugin/modules/NoAttackCooldownModule.java
+++ src/main/java/me/sm/apvpplugin/modules/NoAttackCooldownModule.java
@@ -6,7 +6,6 @@ import org.bukkit.attribute.Attribute;
 import org.bukkit.attribute.AttributeInstance;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
-import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
 
 import java.util.Objects;
