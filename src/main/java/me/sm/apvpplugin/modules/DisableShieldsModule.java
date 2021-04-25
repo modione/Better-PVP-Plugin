2e6ea4fb672e68184cd08e7c6373a76489125d4f
someron <devsomeron@gmail.com>
2021-04-25 16:36:34 +0200

    Cleaned up imports

--- src/main/java/me/sm/apvpplugin/modules/DisableShieldsModule.java
+++ src/main/java/me/sm/apvpplugin/modules/DisableShieldsModule.java
@@ -5,13 +5,9 @@ import net.md_5.bungee.api.ChatColor;
 import net.md_5.bungee.api.ChatMessageType;
 import net.md_5.bungee.api.chat.TextComponent;
 import org.bukkit.Material;
-import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
-import org.bukkit.event.inventory.InventoryDragEvent;
-import org.bukkit.event.inventory.InventoryType;
 import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.inventory.ItemStack;
-import org.bukkit.inventory.PlayerInventory;
 
 public class DisableShieldsModule extends AbstractModule {
 
