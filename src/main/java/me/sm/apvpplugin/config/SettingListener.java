abd4d5b1d9fda8f12551b91464b6a9fce9b797e4
modione <unconfigured@null.spigotmc.org>
2021-04-25 15:10:20 +0200

    ...

--- src/main/java/me/sm/apvpplugin/config/SettingListener.java
+++ src/main/java/me/sm/apvpplugin/config/SettingListener.java
@@ -1,6 +1,5 @@
 package me.sm.apvpplugin.config;
 
-import me.modione.backpackplugin.utils.Setting;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.inventory.InventoryClickEvent;
