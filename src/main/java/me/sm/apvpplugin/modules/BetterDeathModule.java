cd89ed382ce5599d236c7272a0ed05427ebe07a5
someron <devsomeron@gmail.com>
2021-04-25 17:47:23 +0200

    Added Spawn protection timer

--- src/main/java/me/sm/apvpplugin/modules/BetterDeathModule.java
+++ src/main/java/me/sm/apvpplugin/modules/BetterDeathModule.java
@@ -20,14 +20,16 @@ public class BetterDeathModule extends AbstractModule {
     private Sound soundSelf;
     private String title;
     private String subtitle;
-    private int timeout;
+    private long timeout;
+    private long protectionTime;
 
     public BetterDeathModule(FileConfig config) {
         soundOther = Sound.valueOf(config.getString("better-death.sound-other"));
         soundSelf = Sound.valueOf(config.getString("better-death.sound-self"));
         title = config.getString("better-death.title");
         subtitle = config.getString("better-death.subtitle");
-        timeout = config.getInt("better-death.timeout");
+        timeout = config.getLong("better-death.timeout");
+        protectionTime = config.getLong("better-death.protection-time");
     }
 
     @EventHandler
@@ -47,6 +49,13 @@ public class BetterDeathModule extends AbstractModule {
         Location spawn = player.getBedSpawnLocation() == null ? player.getWorld().getSpawnLocation() : player.getBedSpawnLocation();
         if(timeout <= 0) {
             player.teleport(spawn);
+            if(!player.isInvulnerable()) {
+                player.setInvulnerable(true);
+                Bukkit.getScheduler().runTaskLater(ApvpPlugin.instance, () -> {
+                    if(!player.isOnline()) return;
+                    player.setInvulnerable(false);
+                }, protectionTime * 20L);
+            }
         } else {
             GameMode before = player.getGameMode();
             player.setGameMode(GameMode.SPECTATOR);
@@ -54,6 +63,13 @@ public class BetterDeathModule extends AbstractModule {
                 if(!player.isOnline()) return;
                 player.teleport(spawn);
                 player.setGameMode(before);
+                if(!player.isInvulnerable()) {
+                    player.setInvulnerable(true);
+                    Bukkit.getScheduler().runTaskLater(ApvpPlugin.instance, () -> {
+                        if(!player.isOnline()) return;
+                        player.setInvulnerable(false);
+                    }, protectionTime * 20L);
+                }
             }, timeout * 20L);
         }
         player.playSound(player.getLocation(), soundSelf, SoundCategory.PLAYERS, 1.0f, 1.0f);
